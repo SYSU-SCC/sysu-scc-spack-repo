e4d4ab36ef92ccfc0154a86718881dfe31323972
WuK <i@wu-kan.cn>
2024-01-14 14:10:58 +0800

    Update init-env.sh

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -1,5 +1,5 @@
 python3 -c "import tarfile;from urllib.request import urlopen;tarfile.open(mode='r|gz',fileobj=urlopen('https://github.com/spack/spack/archive/refs/tags/$1.tar.gz')).extractall('$(dirname $SCC_SETUP_ENV)/../../../spack-$1')"
-mv -R $(dirname $SCC_SETUP_ENV)/../../../spack-$1/spack-$1 $(dirname $SCC_SETUP_ENV)/../../../spack
+mv $(dirname $SCC_SETUP_ENV)/../../../spack-$1/spack-$1 $(dirname $SCC_SETUP_ENV)/../../../spack
 rm -rf $(dirname $SCC_SETUP_ENV)/../../../spack-$1
 . $SCC_SETUP_ENV
 spack mirror add $1 https://binaries.spack.io/$1
