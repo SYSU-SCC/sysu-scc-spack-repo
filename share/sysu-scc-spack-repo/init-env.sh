6c5beb857a1ff03b1bdacbc2a66e3c7f941cfacc
wu-kan <i@wu-kan.cn>
2024-01-14 16:33:46 +0800

    try clang-14

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -1,8 +1,8 @@
-python3 -c "import tarfile;from urllib.request import urlopen;tarfile.open(mode='r|gz',fileobj=urlopen('https://github.com/spack/spack/archive/refs/tags/$1.tar.gz')).extractall('$(dirname $SCC_SETUP_ENV)/../../../spack-$1')"
+python3 -c "from tarfile import open;from urllib.request import urlopen;open(mode='r:gz',fileobj=urlopen('https://github.com/spack/spack/archive/refs/tags/$1.tar.gz')).extractall('$(dirname $SCC_SETUP_ENV)/../../../spack-$1')"
 mv $(dirname $SCC_SETUP_ENV)/../../../spack-$1/spack-* $(dirname $SCC_SETUP_ENV)/../../../spack
 rm -rf $(dirname $SCC_SETUP_ENV)/../../../spack-$1
 . $SCC_SETUP_ENV
 spack mirror add $1 https://binaries.spack.io/$1
 spack buildcache keys --install --trust
-spack repo add --scope=site $(dirname $SCC_SETUP_ENV)/../..
 spack compiler add --scope=site
+spack repo add --scope=site $(dirname $SCC_SETUP_ENV)/../..
