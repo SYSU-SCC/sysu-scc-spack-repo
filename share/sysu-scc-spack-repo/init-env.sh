93fd10a86017781404553678f702db6bfea7945e
wu-kan <i@wu-kan.cn>
2024-01-14 21:43:10 +0800

    update init-env.sh

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -1,4 +1,4 @@
-python3 -c "from tarfile import open;from urllib.request import urlopen;open(mode='r:gz',fileobj=urlopen('https://github.com/spack/spack/archive/refs/tags/$1.tar.gz')).extractall('$(dirname $SCC_SETUP_ENV)/../../../spack-$1')"
+python3 -c "from tarfile import open;from urllib.request import urlopen;open(mode='r|gz',fileobj=urlopen('https://github.com/spack/spack/archive/refs/tags/$1.tar.gz')).extractall('$(dirname $SCC_SETUP_ENV)/../../../spack-$1')"
 mv $(dirname $SCC_SETUP_ENV)/../../../spack-$1/spack-* $(dirname $SCC_SETUP_ENV)/../../../spack
 rm -rf $(dirname $SCC_SETUP_ENV)/../../../spack-$1
 . $SCC_SETUP_ENV
