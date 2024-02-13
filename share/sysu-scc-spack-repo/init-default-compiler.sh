ba30fbcefb677f4b924d06bcfa7aa756c1b4f31e
WuK <i@wu-kan.cn>
2024-02-13 11:14:09 +0800

    Update init-default-compiler.sh

--- share/sysu-scc-spack-repo/init-default-compiler.sh
+++ share/sysu-scc-spack-repo/init-default-compiler.sh
@@ -1,4 +1,4 @@
-spack install --fail-fast --use-buildcache only -y gcc@12.3.0%gcc target=x86_64_v3 && spack gc -y && spack clean -ab
+spack install --fail-fast --use-buildcache only -y gcc@12.3.0%gcc@11.4.0 arch=linux-ubuntu22.04-x86_64_v3 && spack gc -y && spack clean -ab
 echo ">>> _ <<<"
 spack compiler add --scope=site $(spack location -i gcc)
 spack config --scope=site add "packages:all:compiler:[gcc]"
