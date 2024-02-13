bd9cc0e0d1433061a6f9ec42ebb3a18b60b9ad46
WuK <i@wu-kan.cn>
2024-02-13 14:14:16 +0800

    Update init-default-compiler.sh

--- share/sysu-scc-spack-repo/init-default-compiler.sh
+++ share/sysu-scc-spack-repo/init-default-compiler.sh
@@ -1,3 +1,3 @@
-spack buildcache install --otherarch -y gcc@12.3.0 target=x86_64_v3 && spack gc -y && spack clean -ab
-spack compiler add --scope=site $(spack location -i gcc@12.3.0)
-spack config --scope=site add "packages:all:compiler:[gcc@12.3.0]"
+spack buildcache install --otherarch $1 && spack gc -y && spack clean -ab
+spack compiler add --scope=site $(spack location -i $1)
+spack config --scope=site add "packages:all:compiler:[$2]"
