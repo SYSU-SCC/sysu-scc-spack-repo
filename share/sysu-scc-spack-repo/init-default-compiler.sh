f21ea9c120fefeac4d622797ca4cc2ef95056f39
WuK <i@wu-kan.cn>
2024-02-13 16:42:49 +0800

    Update init-default-compiler.sh

--- share/sysu-scc-spack-repo/init-default-compiler.sh
+++ share/sysu-scc-spack-repo/init-default-compiler.sh
@@ -1,6 +1,6 @@
 spack buildcache install --otherarch "$1"
 spack compiler add --scope=site $(spack location -i "$1")
-spack install --fail-fast -y"$2"
+spack install --fail-fast -y "$2" && spack gc -y && spack clean -ab
 rm $(dirname $SCC_SETUP_ENV)/../../../spack/etc/spack/compilers.yaml
 spack compiler add --scope=site $(spack location -i "$2")
 spack config --scope=site add "packages:all:compiler:[$3]"
