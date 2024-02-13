0110cbd030840470c65921432ac7f26eba4be29b
WuK <i@wu-kan.cn>
2024-02-13 11:22:34 +0800

    Update init-env.sh

--- share/sysu-scc-spack-repo/init-env.sh
+++ share/sysu-scc-spack-repo/init-env.sh
@@ -2,7 +2,8 @@ python3 -c "from tarfile import open;from urllib.request import urlopen;open(mod
 mv $(dirname $SCC_SETUP_ENV)/../../../spack-$1/spack-* $(dirname $SCC_SETUP_ENV)/../../../spack
 rm -rf $(dirname $SCC_SETUP_ENV)/../../../spack-$1
 . $SCC_SETUP_ENV
-spack mirror add $1 https://binaries.spack.io/$1
+spack mirror add binaries-spack-io-$1 https://binaries.spack.io/$1
+spack mirror add E4S https://cache.e4s.io
 spack buildcache keys --install --trust
 spack compiler add --scope=site
 spack repo add --scope=site $(dirname $SCC_SETUP_ENV)/../..
