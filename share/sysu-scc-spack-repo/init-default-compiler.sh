f4130539facead8fde1af64ddec3ca01bdfcb13c
wu-kan <github@wu-kan.cn>
2024-11-20 20:28:04 +0800

    update

--- share/sysu-scc-spack-repo/init-default-compiler.sh
+++ share/sysu-scc-spack-repo/init-default-compiler.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-spack buildcache install --otherarch "$1"
+spack buildcache install --unsigned --otherarch "$1"
 spack compiler add --scope=site $(spack location -i "$1")
 spack install --fresh --fail-fast -y "$2" && spack gc -y && spack clean -ab
 rm $(dirname $SCC_SETUP_ENV)/../../../spack/etc/spack/compilers.yaml
