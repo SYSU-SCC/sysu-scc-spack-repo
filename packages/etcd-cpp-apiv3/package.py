90fd679ba2a01ad4539fa8385ab20ba08bfbea76
wu-kan <github@wu-kan.cn>
2025-08-12 16:49:32 +0800

    Update

--- packages/etcd-cpp-apiv3/package.py
+++ packages/etcd-cpp-apiv3/package.py
@@ -20,13 +20,15 @@ class EtcdCppApiv3(CMakePackage):
     version("master", branch="master")
     version("0.15.4", sha256="4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4")
 
+    depends_on("c")
+    depends_on("cxx")
     depends_on("boost")
-    depends_on("openssl", type=["build", "link"])
-    depends_on("grpc", type=["build", "link"])
-    depends_on("protobuf", type=["build", "link"])
-    depends_on("cpprestsdk", type=["build", "link"])
+    depends_on("openssl")
+    depends_on("grpc")
+    depends_on("protobuf")
+    depends_on("cpprestsdk")
 
-    patch( # Latest GRPC >= 2.66 has dropped GRPC_ASSERT macro
-        "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/commit/216b86f8d763acf88e4ed7265f983b57c12da2df.patch",
-        sha256="9a882c50ce9105eaad6214777bf44d6302ae3c5fa6800fa7c9d58a611bb2f420",
+    patch(  # Latest GRPC >= 2.66 has dropped GRPC_ASSERT macro
+        "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/commit/216b86f8d763acf88e4ed7265f983b57c12da2df.patch?full_index=1",
+        sha256="4d16ab69a5dd6af4c0eb99c777dae6ffb913116b074882381cea69889789308e",
     )
